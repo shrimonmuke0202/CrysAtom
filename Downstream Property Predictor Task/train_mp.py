b4932c11beea44eb9b14e2387e9547d46745927d
Shrimon Mukherjee <71223690+shrimonmuke0202@users.noreply.github.com>
2024-11-29 14:41:04 +0530

    Delete Downstream Property Predictor Task/train_mp.py

--- Downstream Property Predictor Task/train_mp.py
+++
@@ -1,7 +0,0 @@
-from matformer.train_props import train_prop_model 
-props = [
-    "e_form",
-    "gap pbe",
-]
-train_prop_model(learning_rate=0.001,name="matformer", dataset="megnet", prop=props[0], pyg_input=True, n_epochs=500, batch_size=64, use_lattice=True, output_dir="./demo", use_angle=False, save_dataloader=False, atom_features = "./dense_vector/200_dimensional_vector.pkl",random_seed=123,
-                )
